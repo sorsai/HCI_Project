1d0111a9d3f04e6e178ee457b00aca8d9f67b6dd
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2018-12-28 14:31:46 +0700

    js for range slider

--- js/customs.js
+++ js/customs.js
@@ -8,3 +8,25 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+function login(){
+                $(document).ready(function(){
+                $("button").click(function(){
+                $("#login").load("login.html");
+                    });
+                });
+              }
+
+
+
+  // With JQuery
+$("#ex6").slider();
+$("#ex6").on("slide", function(slideEvt) {
+	$("#ex6SliderVal").text(slideEvt.value);
+});
+
+// Without JQuery
+var slider = new Slider("#ex6");
+slider.on("slide", function(sliderValue) {
+	document.getElementById("ex6SliderVal").textContent = sliderValue;
+});
