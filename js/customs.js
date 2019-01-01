51d10cbf15f2218223bc15cb45492fef3eb47fc0
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2019-01-01 20:35:27 +0700

    pop up login

--- js/customs.js
+++ js/customs.js
@@ -30,3 +30,49 @@ var slider = new Slider("#ex6");
 slider.on("slide", function(sliderValue) {
 	document.getElementById("ex6SliderVal").textContent = sliderValue;
 });
+
+
+$(document).on("pagecreate", function () {
+  $("#foo").on("click", function () {
+      // close button
+      var closeBtn = $('<a href="#" data-rel="back" class="ui-btn-right ui-btn ui-btn-b ui-corner-all ui-btn-icon-notext ui-icon-delete ui-shadow">Close</a>');
+
+      // text you get from Ajax
+      var content = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing. Morbi convallis sem et dui sollicitudin tincidunt.</p>";
+
+      // Popup body - set width is optional - append button and Ajax msg
+      var popup = $("<div/>", {
+          "data-role": "popup"
+      }).css({
+          width: $(window).width() / 2.5 + "px",
+          padding: 5 + "px"
+      }).append(closeBtn).append(content);
+
+      // Append it to active page
+      $.mobile.pageContainer.append(popup);
+
+      // Create it and add listener to delete it once it's closed
+      // open it
+      $("[data-role=popup]").popup({
+          dismissible: false,
+          history: false,
+          theme: "b",
+          /* or a */
+          positionTo: "window",
+          overlayTheme: "b",
+          /* "b" is recommended for overlay */
+          transition: "pop",
+          beforeposition: function () {
+              $.mobile.pageContainer.pagecontainer("getActivePage")
+                  .addClass("blur-filter");
+          },
+          afterclose: function () {
+              $(this).remove();
+              $(".blur-filter").removeClass("blur-filter");
+          },
+          afteropen: function () {
+              /* do something */
+          }
+      }).popup("open");
+  });
+});
