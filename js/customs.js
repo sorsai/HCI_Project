e981ee138a73c283ae108eb37a9a9d146d90ccf6
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2019-01-02 11:47:01 +0700

    filtering in findclass

--- js/customs.js
+++ js/customs.js
@@ -76,3 +76,33 @@ $(document).on("pagecreate", function () {
       }).popup("open");
   });
 });
+
+
+$(function () {
+  $("div").slice(0, 4).show();
+  $("#loadMore").on('click', function (e) {
+      e.preventDefault();
+      $("div:hidden").slice(0, 4).slideDown();
+      if ($("div:hidden").length == 0) {
+          $("#load").fadeOut('slow');
+      }
+      $('html,body').animate({
+          scrollTop: $(this).offset().top
+      }, 1500);
+  });
+});
+
+$('a[href=#top]').click(function () {
+  $('body,html').animate({
+      scrollTop: 0
+  }, 600);
+  return false;
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+      $('.totop a').fadeIn();
+  } else {
+      $('.totop a').fadeOut();
+  }
+});
